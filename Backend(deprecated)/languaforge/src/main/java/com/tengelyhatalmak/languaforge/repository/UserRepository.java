c984009bbd9df1dad4722dca601d66e052015fd3
Naszvadi-Széll Soma <97170061+Naszisomi@users.noreply.github.com>
2026-01-08 12:40:31 +0100

    Delete Backend(deprecated) directory

--- Backend(deprecated)/languaforge/src/main/java/com/tengelyhatalmak/languaforge/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tengelyhatalmak.languaforge.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tengelyhatalmak.languaforge.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
